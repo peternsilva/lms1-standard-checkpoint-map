0a7ed2fbe3c8ac317d4f1b024d0ac4eb56838baf
Pete Silva <peter.silva@galvanize.com>
2018-03-26 09:55:58 -0600

    attempt to fix monkeyscript which wasn't making ajax requests unless Chrome dev tools was open

--- public/js/monkeyscript.js
+++ public/js/monkeyscript.js
@@ -14,7 +14,7 @@
 
 
 // Main Exec
-$(document).ready(() => {
+$(document).ready( function() {
   console.log('Standard 2 Checkpoint script started.');
   var domain = 'learn-check-standards.herokuapp.com';
   var cohortId = 372;   // Only seems to work for g81
@@ -25,16 +25,20 @@ $(document).ready(() => {
     if ($(this).attr('href')) {
       var standardId = $(this).attr('href').substring(1);
 
-      //
-      fetch(`https://${domain}/checkpoints/cohort/${cohortId}/standard/${standardId}`)
-      .then((res) => {
-        // if problem, bail
-        if (res.status !== 200) {
-          throw new Error(`Couldn't find checkpoint for standard:${standardId}`);
+      // Wait a little before hammering server
+      setTimeout(() => {
+        //
+        var ajaxUrl = `https://${domain}/checkpoints/cohort/${cohortId}/standard/${standardId}`;
+        // console.log('ajaxUrl', ajaxUrl);
+        fetch(ajaxUrl)
+        .then((res) => {
+          // console.log('Fetched:', res);
+          // if problem, bail
+          if (res.status !== 200) {
+            throw new Error(`Couldn't find checkpoint for standard:${standardId}`);
 
-        } else {
-          // parse response to JSON then deal with response data
-          if(res) {
+          } else {
+            // parse response to JSON then deal with response data
             var json = res.json();
             if(json) {
               json.then((data) => {
@@ -46,19 +50,19 @@ $(document).ready(() => {
                   throw new Error(`Couldn't find checkpoint for standard:${standardId}`);
                 }
 
+                // console.log('this',this);
                 $(this).before(`<a href="${data.link}" style="margin-right:5px;font-size: 150%;" target="_blank">&#9099;</a>`);
               });
             }
-
           }
+        })
+        .catch((err) => {
+          //do nada
+        });
+        //
+      }, 500);
 
 
-        }
-      })
-      .catch((err) => {
-        //nada
-      });
-      //
 
 
     }
